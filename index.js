4972a2dfbcbf5cac2e5477aa02e2449ccba04b0d
ledbetterljoshua <ledbetterljoshua@gmail.com>
2019-06-13 10:50:19 -0500

    adds animations

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT || 3001; 
+const port = process.env.PORT || 6001; 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
